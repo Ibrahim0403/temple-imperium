6a0745221e23c62af6afce225777d11892ecdcba
ajaz842 <61790529+ajaz842@users.noreply.github.com>
2020-07-31 02:54:55 +0100

    Update with menu and sound

---
+++ temple-imperium-master/Temple Imperium GH/Assets/Scripts/SettingsScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsScript : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public void VolumeSlider (float volume)
+    {
+        mixer.SetFloat("Sound", volume);
+    }
+}
